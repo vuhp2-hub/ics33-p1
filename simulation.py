f4a8429044fcea24a602eb2a4151683b80f64116
Vu Huy Minh Pham <vuhp2@uci.edu>
2026-01-27 16:09:10 -0800

    add: adding untested code

--- simulation.py
+++ simulation.py
@@ -5,18 +5,27 @@ class Device:
     _id_count = 0
     def __init__(self):
         '''Construct Device object. Id increment from class attribute'''
-        self.alerts = []
+        self._alerts = []
         Device._id_count += 1
         self._id = Device._id_count
     def get_id(self):
         '''Returns device's id'''
         return self._id
-    def send_alert(self, message: str, time_received: int, device: Device):
-        '''Send a new alert'''
-        pass
-    def accept_alert(self, message: str, time_received: int, device: Device):
+    def send_alert(self, message: str, time_began: int, receiver: Device, time_to_receive: int):
+        assert receiver, "No Device as argument"
+        alert = Alert(message, time_began)
+        alert.add_recipient(receiver)
+        self._alerts.append(alert)
+        receiver.accept_alert(message, time_to_receive, self)
+    def accept_alert(self, message: str, time_received: int, sender: Device):
         '''Creates the object's own alert'''
-        pass
+        assert sender, "No Device as argument"
+        alert = Alert(message, time_received)
+        self._alerts.append(alert)
+    def cancel_alert(self, alert_message: str):
+        for alert in self._alerts:
+            if alert.get_message() != alert_message: continue
+            alert.cancel()
 
 class Alert:
     '''
@@ -35,6 +44,7 @@ class Alert:
         self._message = message
         self._time = time
         self._cancelled = False
+        self._recipients = []
     def get_message(self) -> str:
         '''Returns the alert's message'''
         return self._message
@@ -42,10 +52,17 @@ class Alert:
         '''Returns the alert's time'''
         return self._time
     def cancel(self) -> bool:
+        '''
+        Cancels the alert
+        '''
         if self._cancelled: return False
         self._cancelled = True
         return True
     def is_cancelled(self) -> bool:
+        '''Returns a boolean telling of the alert is cancelled or not'''
         return self._cancelled
+    def add_recipient(self, device: Device):
+        '''Add a recipient (a device) to the list of recipients'''
+        self._recipients.append(device)
     
 
