84d3b4df3260ef686a6f85515ed41b37b520e25f
Vu Huy Minh Pham <vuhp2@uci.edu>
2026-01-27 17:31:58 -0800

    add: send_alert, cancel_art tests; untested
    untested: Device.propagate()

--- simulation.py
+++ simulation.py
@@ -11,58 +11,76 @@ class Device:
     def get_id(self):
         '''Returns device's id'''
         return self._id
-    def send_alert(self, message: str, time_began: int, receiver: Device, time_to_receive: int):
-        assert receiver, "No Device as argument"
-        alert = Alert(message, time_began)
-        alert.add_recipient(receiver)
+    def alert(self, description: str, time_began: int) -> Alert:
+        '''Creates a new alert given name and time begun'''
+        assert description and time_began, "Missing arguments"
+        # Make Alerts (Or Blank alert due to cancellation) conflict and do nothing
+        for existing_alert in self._alerts:
+            if existing_alert.get_description() == description:
+                existing_alert.change_time(time_began)
+                return existing_alert
+        alert = Alert(description, time_began)
         self._alerts.append(alert)
-        receiver.accept_alert(message, time_to_receive, self)
-    def accept_alert(self, message: str, time_received: int, sender: Device):
-        '''Creates the object's own alert'''
-        assert sender, "No Device as argument"
-        alert = Alert(message, time_received)
-        self._alerts.append(alert)
-    def cancel_alert(self, alert_message: str):
+        return alert
+    def cancel_alert(self, description: str, time_cancelled: int):
+        '''Cancels alert of the description
+        time_cancelled is the time the device is supposed to receive the cancellation
+        '''
+        assert description and time_cancelled, "Missing arguments"
+        alert_exists = False
         for alert in self._alerts:
-            if alert.get_message() != alert_message: continue
-            alert.cancel()
-
+            if alert.get_description() != description: continue
+            alert_exists = True
+            if alert.get_time() >= time_cancelled: return
+            alert.cancel(time_cancelled)
+        if alert_exists: return
+        # Cancellation arrives early? --> Sets up blank alert
+        alert = self.alert(description, time_cancelled)
+        alert.cancel(time_cancelled)
+    def propagate(self, receiver: Device, time_received: int):
+        '''Propagates alerts'''
+        assert receiver, "No Device receiver as argument"
+        for alert in self._alerts:
+            alert_message = alert.get_description()
+            if alert.is_cancelled():
+                receiver.cancel_alert(alert_message, time_received)
+            else:
+                receiver.alert(alert_message, time_received)
 class Alert:
     '''
     Alert class.
     An Alert class does not have an id because its alert's content
-    is compared for the same message. If another alert with
-    the same message is created, the manager of the collection
-    of alerts should delete the alert with the same message.
+    is compared for the same description. For now, alert of the same description
+    are allowed to coexist
 
     The attribute time is unique. Each Device should have their own
     alert. The _time attribute either means the time an alert
     was sent, or the time the alert was received.
     '''
-    def __init__(self, message: str, time: int):
+    def __init__(self, description: str, time: int):
         '''Construct Alert object'''
-        self._message = message
+        self._description = description
         self._time = time
         self._cancelled = False
-        self._recipients = []
-    def get_message(self) -> str:
-        '''Returns the alert's message'''
-        return self._message
+        self._propagation_ceased = False
+    def get_description(self) -> str:
+        '''Returns the alert's description'''
+        return self._description
     def get_time(self) -> int:
         '''Returns the alert's time'''
         return self._time
-    def cancel(self) -> bool:
+    def cancel(self, time_cancelled: int) -> bool:
         '''
         Cancels the alert
         '''
         if self._cancelled: return False
         self._cancelled = True
+        self._time = time_cancelled
         return True
     def is_cancelled(self) -> bool:
         '''Returns a boolean telling of the alert is cancelled or not'''
         return self._cancelled
-    def add_recipient(self, device: Device):
-        '''Add a recipient (a device) to the list of recipients'''
-        self._recipients.append(device)
-    
-
+    def change_time(self, time: int):
+        self._time = time
+    def set_propagation_ceased(self):
+        self._propagation_ceased = True
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -8,17 +8,42 @@ class TestAlert(unittest.TestCase):
         self._alert = Alert("We all say 'Boo'", 1000)
 
     def test_alert_init(self):
-        self.assertEqual(self._alert.get_message(), "We all say 'Boo'")
+        self.assertEqual(self._alert.get_description(), "We all say 'Boo'")
         self.assertEqual(self._alert.get_time(), 1000)
 
     def test_cancel_alert(self):
-        self._alert.cancel()
+        self._alert.cancel(2000)
         self.assertEqual(self._alert.is_cancelled(), True)
+        self.assertEqual(self._alert.get_time(), 2000)
 
 class TestDevice(unittest.TestCase):
     def setUp(self):
+        Device._id_count = 0
         self._devices = [Device(), Device(), Device()]
     def test_devices_init(self):
         self.assertEqual(Device._id_count, 3)
         for i in range(len(self._devices)):
             self.assertEqual(self._devices[i].get_id(), i+1)
+    def test_device_alert(self):
+        device = self._devices[0]
+        device.alert("Hello", 1000)
+        self.assertTrue(device._alerts)
+        self.assertEqual(device._alerts[0].get_time(), 1000)
+    def test_alert_same_description_no_duplicate_time_updated(self):
+        device = self._devices[0]
+        device.alert("Hello", 1000)
+        device.alert("Hello", 2000)
+        self.assertEqual(len(device._alerts), 1)
+        self.assertEqual(device._alerts[0].get_time(), 2000)
+    def test_cancel_alert(self):
+        device = self._devices[0]
+        device.alert("Hello", 1000)
+        self.assertEqual(device._alerts[0].get_time(), 1000)
+        device.cancel_alert("Hello", 2000)
+        self.assertTrue(device._alerts[0].is_cancelled())
+        self.assertEqual(device._alerts[0].get_time(), 2000)
+    def test_cancel_alert_no_existing_alert(self):
+        device = self._devices[0]
+        device.cancel_alert("Hello", 2000)
+        self.assertTrue(device._alerts[0].is_cancelled())
+        self.assertEqual(device._alerts[0].get_time(), 2000)
