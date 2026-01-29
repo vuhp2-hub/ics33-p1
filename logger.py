7a059b18f4918125d8a8951ef117a2e9e78aa1b5
Vu Huy Minh Pham <vuhp2@uci.edu>
2026-01-28 23:34:08 -0800

    add: logger organized

--- logger.py
+++ logger.py
@@ -4,19 +4,34 @@ class Logger:
     '''This class logs the simulation'''
     def __init__(self, length: int):
         self._time_log = {}
-        self._sorted_time_keys = {}
+        self._sorted_time_keys = []
         self._length = length
     def log_received(self, time: int, alert_type: str, recipient: int, sender: int, description: str):
+        '''Logs recieved'''
         assert alert_type and (alert_type == "CANCELLATION" or alert_type == "ALERT")
         assert description
         if not str(time) in self._time_log:
+            self._sorted_time_keys.append(time)
+            self._sorted_time_keys.sort()
             self._time_log[time] = []
         self._time_log[time].append(f'#{recipient} RECEIVED {alert_type} FROM #{sender}: {description}')
 
     def log_sent(self, time: int, alert_type: str, sender: int, recipient: int, description: str):
+        '''Logs sent'''
         assert alert_type and (alert_type == "CANCELLATION" or alert_type == "ALERT")
         assert description
         if not str(time) in self._time_log:
+            self._sorted_time_keys.append(time)
+            self._sorted_time_keys.sort()
             self._time_log[time] = []
         self._time_log[time].append(f'#{sender} SENT {alert_type} TO #{recipient}: {description}')       
-    
+
+    def organize_log(self) -> str:
+        '''Organize log and return it'''
+        result = ''
+        for time_key in self._sorted_time_keys:
+            assert time_key in self._time_log
+            for time_log in self._time_log[time_key]:
+                result += f'@{time_key}: {time_log}\n'
+        result += f'@{self._length}: END'
+        return result
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -12,3 +12,7 @@ class TestLogger(unittest.TestCase):
     def test_log_sent(self):
         self._logger.log_sent(1000, "ALERT", 1, 2, "Hi")
         self.assertEqual(self._logger._time_log[1000][0], '#1 SENT ALERT TO #2: Hi')
+    def test_logger_organized(self):
+        self._logger.log_sent(1000, "ALERT", 1, 2, "Hi")
+        self._logger.log_received(2000, "ALERT", 2, 1, "Hi")
+        self.assertEqual(self._logger.organize_log(), "@1000: #1 SENT ALERT TO #2: Hi\n@2000: #2 RECEIVED ALERT FROM #1: Hi\n@100000: END")
