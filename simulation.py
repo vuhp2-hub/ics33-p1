ce353711c23941bb47e7ca85ebd0a30645b4b859
Vu Huy Minh Pham <vuhp2@uci.edu>
2026-01-28 20:20:53 -0800

    add: finished simulation + propagation test
    add: simulation.py cancelling implemented
    
    add: cancelling fixed
    
    add: propagation cancellation fixed 3

--- simulation.py
+++ simulation.py
@@ -8,45 +8,76 @@ class Device:
         self._alerts = []
         Device._id_count += 1
         self._id = Device._id_count
+        self._prop_rules = []
+        self._cancellation_times = {}
+    def add_prop_rule(self, receiver: Device, delay: int):
+        '''Add popragation given device and delay'''
+        assert receiver and type(delay) == int, "add_prop_rule: missing arguments device and delay"
+        self._prop_rules.append((receiver, delay))
     def get_id(self):
         '''Returns device's id'''
         return self._id
-    def alert(self, description: str, time_began: int) -> Alert:
+    def alert(self, description: str, time_began: int, no_propagate=False) -> Alert:
         '''Creates a new alert given name and time begun'''
         assert description and type(time_began) == int, "Missing arguments"
         # Make Alerts (Or Blank alert due to cancellation) conflict and do nothing
-        for existing_alert in self._alerts:
-            if existing_alert.get_description() == description:
-                existing_alert.change_time(time_began)
-                return existing_alert
-        alert = Alert(description, time_began)
-        self._alerts.append(alert)
-        return alert
-    def cancel_alert(self, description: str, time_cancelled: int):
-        '''Cancels alert of the description
+        existing_alert = None
+        for alert in self._alerts:
+            if alert.get_description() == description:
+                alert.change_time(time_began)
+                existing_alert = alert
+        if not existing_alert:
+            existing_alert = Alert(description, time_began)
+            if not no_propagate:
+                self._alerts.append(existing_alert)
+        if not no_propagate:
+            self.propagate(existing_alert, time_began)
+        return existing_alert
+    def _cancel_alert(self, description: str, time_cancelled: int):
+        '''
+        Cancels alert of the description.
         time_cancelled is the time the device is supposed to receive the cancellation
         '''
         assert description and type(time_cancelled) == int, "Missing arguments"
-        alert_exists = False
-        for alert in self._alerts:
+        for i in range(len(self._alerts)):
+            alert = self._alerts[i]
             if alert.get_description() != description: continue
-            alert_exists = True
-            if alert.get_time() >= time_cancelled: return
+            assert alert.get_time() < time_cancelled
+            if alert.is_cancelled(): return
             alert.cancel(time_cancelled)
-        if alert_exists: return
-        # Cancellation arrives early? --> Sets up blank alert
-        alert = self.alert(description, time_cancelled)
+            self.propagate(alert, time_cancelled)
+            return
+        # assume alert has not have time to be created
+        alert = self.alert(description, time_cancelled, no_propagate=True)
         alert.cancel(time_cancelled)
-    def propagate(self, receiver: Device, time_delay: int):
-        '''Propagates alerts'''
-        assert receiver, "No Device receiver as argument"
-        for alert in self._alerts:
-            alert_message = alert.get_description()
-            time_received = alert.get_time() + time_delay
-            if alert.is_cancelled():
-                receiver.cancel_alert(alert_message, time_received)
+        self._alerts.append(alert)
+        self.propagate(alert, time_cancelled)
+    def propagate(self, alert: Alert, time_began: int):
+        '''Propagates alert if not ceased'''
+        if alert.is_propagation_ceased(): return
+
+        alert_desc = alert.get_description()
+
+        for prop_rule in self._prop_rules:
+            receiver = prop_rule[0]
+            delay = prop_rule[1]
+            time_received = time_began + delay
+            if alert_desc in self._cancellation_times:
+                receiver._cancellation_times[alert_desc] = self._cancellation_times[alert_desc]
+                if self._cancellation_times[alert_desc] < time_received:
+                    if not alert.is_cancelled():
+                        self._cancel_alert(alert_desc, time_received)
+                    else:
+                        receiver._cancel_alert(alert_desc, time_received)
+                else:
+                    receiver.alert(alert_desc, time_received)
             else:
-                receiver.alert(alert_message, time_received)
+                receiver.alert(alert_desc, time_received)
+
+        if alert.is_cancelled():
+            alert.set_propagation_ceased()
+    def set_cancellation_time(self, description: str, time: int):
+        self._cancellation_times[description] = time
 class Alert:
     '''
     Alert class.
@@ -85,3 +116,5 @@ class Alert:
         self._time = time
     def set_propagation_ceased(self):
         self._propagation_ceased = True
+    def is_propagation_ceased(self):
+        return self._propagation_ceased
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -35,27 +35,33 @@ class TestDevice(unittest.TestCase):
         device.alert("Hello", 2000)
         self.assertEqual(len(device._alerts), 1)
         self.assertEqual(device._alerts[0].get_time(), 2000)
-    def test_cancel_alert(self):
-        device = self._devices[0]
-        device.alert("Hello", 1000)
-        self.assertEqual(device._alerts[0].get_time(), 1000)
-        device.cancel_alert("Hello", 2000)
-        self.assertTrue(device._alerts[0].is_cancelled())
-        self.assertEqual(device._alerts[0].get_time(), 2000)
-    def test_cancel_alert_no_existing_alert(self):
-        device = self._devices[0]
-        device.cancel_alert("Hello", 2000)
-        self.assertTrue(device._alerts[0].is_cancelled())
-        self.assertEqual(device._alerts[0].get_time(), 2000)
     def test_device_propagation(self):
         device1 = self._devices[0]
         device2 = self._devices[1]
         device3 = self._devices[2]
 
+        device1.add_prop_rule(device2, 1000)
+        device2.add_prop_rule(device3, 2000)
+
         device1.alert("Hello", 0)
-        device1.propagate(device2, 1000)
         self.assertTrue(device2._alerts)
+        self.assertTrue(device3._alerts)
         self.assertEqual(device2._alerts[0].get_time(), 1000)
-        device2.propagate(device3, 2000)
         self.assertEqual(device3._alerts[0].get_time(), 3000)
         self.assertEqual(device3._alerts[0].get_description(), "Hello")
+
+    def test_device_propagation_cancelled(self):
+        device1 = self._devices[0]
+        device2 = self._devices[1]
+        device3 = self._devices[2]
+
+        device1.add_prop_rule(device2, 1000)
+        device2.add_prop_rule(device3, 2000)       
+        device3.add_prop_rule(device1, 1000)
+
+        device1.set_cancellation_time("Hello", 1000)
+        device1.alert("Hello", 0)
+
+        self.assertTrue(device3._alerts[0].is_cancelled())
+        self.assertTrue(device1._alerts[0].is_cancelled())
+        self.assertTrue(device2._alerts[0].is_cancelled())
